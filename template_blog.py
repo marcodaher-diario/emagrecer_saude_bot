d897db71cd8ffe3d8140af006bf8202452e01e15
marcodaher-diario <marco.daher@gmail.com>
2026-03-06 01:55:08 -0300

    Update template_blog.py

--- template_blog.py
+++ template_blog.py
@@ -44,7 +44,9 @@ def obter_esqueleto_html(dados):
 
     html = f"""
 <style>
-    h3.post-title, .post-title {{ display: none !important; }}
+    h3.post-title, .post-title {{ style="text-align:center !important; font-size:28px !important; font-weight:bold !important; 
+               margin-bottom:20px !important; text-transform:uppercase !important;"}}
+        {titulo} 
 </style>
 <div style="max-width:900px !important; margin:auto !important; font-family:Arial, sans-serif !important; 
             color:rgb(7, 55, 99) !important; line-height:1.7 !important; text-align:justify !important;">
